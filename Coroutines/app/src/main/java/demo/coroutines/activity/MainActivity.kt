bdff00f9058906ba5d0e65915d47572f0747adff
Totem <majianglin@totemtec.com>
2022-05-11 18:29:18 +0800

    init

---
+++ Coroutines/app/src/main/java/demo/coroutines/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package demo.coroutines.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import demo.coroutines.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
