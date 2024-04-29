e28dbc42a20504c8afbe142fd5e5d1cccf003bd2
Totem <majianglin@totemtec.com>
2024-04-29 11:14:06 +0800

    update Kotlin

--- Kotlin/ReadMe.md
+++ Kotlin/ReadMe.md
@@ -1,4 +1,16 @@
-IDEA 2021.3.2
-JDK 11
-Gradle 7.3
-Kotlin 1.6
+# 开发环境
+- JDK 17
+- IDEA 2024.1
+- Gradle 8.2
+- Kotlin 1.9
+
+# 启用 K2 编译器
+
+转到
+- Settings
+    - Languages & Frameworks
+        - Kotlin（设置 | 语言和框架 | Kotlin）
+        - 点击 Enable the K2-based Kotlin plugin（启用基于 K2 的 Kotlin 插件）复选框启用新模式。
+
+> ** NOTE **  
+> 启用 K2 模式后，您需要重新启动 IDE。
--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,30 +1,19 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.6.21"
-    application
+    kotlin("jvm") version "1.9.23"
 }
 
-group = "demo"
-version = "1.0"
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-}
-
-application {
-    mainClass.set("MainKt")
+kotlin {
+    jvmToolchain(17)
 }
